647f8eb9e9b1043b9599dbc760e5cf1ed05c9d57
Adelwin Bawah Awuni <adelwinbawahawuni@TheAdel-1047.local>
2026-07-03 14:23:57 -0400

    Fix production CORS

--- app/main.py
+++ app/main.py
@@ -1,36 +1,36 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.database import Base, engine
-from app.models import user, ticket  # ensures models register
-
-from app.api import auth, tickets, dashboard
+from app.models import user, ticket
 from app.api import auth, tickets, dashboard, query
-
-from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(
     title="AI Customer Intelligence System",
-    version="1.0.0"
+    version="1.0.0",
 )
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://ai-customer-intelligence-dashboard-flame.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
-# create tables
 Base.metadata.create_all(bind=engine)
 
-# routers
+
 app.include_router(auth.router)
 app.include_router(tickets.router)
-app.include_router(dashboard.router) 
+app.include_router(dashboard.router)
 app.include_router(query.router)
+
 
 @app.get("/")
 def root():
